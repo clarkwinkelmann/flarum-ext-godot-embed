dc500a2fdce0980feb2d64b5aa85eca90a07a4f9
Xwdit <xwditfr@gmail.com>
2022-01-09 00:31:54 +0100

    fix a char typo

--- views/embed.blade.php
+++ views/embed.blade.php
@@ -200,7 +200,7 @@
         const engine = new Engine({
             args: @json($args),
             fileSizes: @json($fileSizes),
-            experimentalVK：@json($virtualkey),
+            experimentalVK: @json($virtualkey),
             onProgress: function (current, total) {
                 if (total > 0) {
                     statusProgressInner.style.width = current / total * 100 + '%';
